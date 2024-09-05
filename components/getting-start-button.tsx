de624cb4680d0d502eb98d2157f674fc18c39392
carlos <cmvm.www@gmail.com>
2024-09-05 17:47:11 -0500

    feat(cambio del nombre del boton)

--- components/getting-start-button.tsx
+++ components/getting-start-button.tsx
@@ -9,7 +9,7 @@ export const GettingStartButton: React.FC = () => {
 
     return (
         <Button onClick={() => push('/login')} className={'mt-5'}>
-            Vamos!
+            Vamos a empezar!
         </Button>
     );
 }
