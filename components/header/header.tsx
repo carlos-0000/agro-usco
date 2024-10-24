307541ca8bf3d602b287e926b8b8edd240cf4e5c
carlos <cmvm.www@gmail.com>
2024-10-24 15:46:17 -0500

    Implement account lock after three failed attempts and improve UI elements
    - Added functionality to lock user accounts after three unsuccessful login attempts. Users are redirected to the account recovery screen and receive a text message to reset their PIN.
    - Improved design of input fields and menu for better user experience.
    - Fixed issue where the logout button did not appear on the mobile version; temporarily added a button for presentation purposes.
    - Created an endpoint for internal analysis functionality.

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,159 +2,169 @@
 
 import { useState } from 'react';
 import {
-  IconBuildingStore,
-  IconChevronDown,
-  IconHeart,
-  IconLogout,
-  IconMessage,
-  IconSettings,
-  IconStar,
+    IconBuildingStore,
+    IconChevronDown,
+    IconHeart,
+    IconLogout,
+    IconMessage,
+    IconSettings,
+    IconStar,
 } from '@tabler/icons-react';
 import cx from 'clsx';
 import { signOut } from 'next-auth/react';
 import {
-  Avatar,
-  Burger,
-  Container,
-  Group,
-  Menu,
-  rem,
-  Tabs,
-  Text,
-  UnstyledButton,
-  useMantineTheme,
+    Avatar,
+    Burger, Button,
+    Container,
+    Group,
+    Menu,
+    rem,
+    Tabs,
+    Text,
+    UnstyledButton,
+    useMantineTheme,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import classes from './header.module.css';
 
 const user = {
-  name: 'Usuario',
-  email: 'janspoon@fighter.dev',
-  image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png',
+    name: 'Usuario',
+    email: 'janspoon@fighter.dev',
+    image: 'https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-1.png',
 };
 
 const tabs = [
-  'Mercado Agro',
+    'Mercado Agro',
     'Productos',
     'Categorias',
     'Ofertas',
 ];
 
 export const Header = () => {
-  const theme = useMantineTheme();
-  const [opened, { toggle }] = useDisclosure(false);
-  const [userMenuOpened, setUserMenuOpened] = useState(false);
+    const theme = useMantineTheme();
+    const [opened, { toggle }] = useDisclosure(false);
+    const [userMenuOpened, setUserMenuOpened] = useState(false);
 
-  const items = tabs.map((tab) => (
-    <Tabs.Tab value={tab} key={tab}>
-      {tab}
-    </Tabs.Tab>
-  ));
+    const items = tabs.map((tab) => (
+        <Tabs.Tab value={tab} key={tab}>
+            {tab}
+        </Tabs.Tab>
+    ));
 
-  return (
-    <div className={classes.header}>
-      <Container className={classes.mainSection} size="md">
-        <Group justify="space-between">
-          <Group>
-            <IconBuildingStore />
-            <Text size="lg" fw="bold">
-              Mercado Agro
-            </Text>
-          </Group>
+    return (
+        <div className={classes.header}>
+            <Container className={classes.mainSection} size="md">
+                <Group justify="space-between">
+                    <Group>
+                        <IconBuildingStore />
+                        <Text size="lg" fw="bold">
+                            Mercado Agro
+                        </Text>
+                    </Group>
 
-          <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+                    <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
 
-          <Menu
-            width={260}
-            position="bottom-end"
-            transitionProps={{ transition: 'pop-top-right' }}
-            onClose={() => setUserMenuOpened(false)}
-            onOpen={() => setUserMenuOpened(true)}
-            withinPortal
-          >
-            <Menu.Target>
-              <UnstyledButton
-                className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
-              >
-                <Group gap={7}>
-                  <Avatar src={user.image} alt={user.name} radius="xl" size={20} />
-                  <Text fw={500} size="sm" lh={1} mr={3}>
-                    {user.name}
-                  </Text>
-                  <IconChevronDown style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
+                    <Menu
+                        width={260}
+                        position="bottom-end"
+                        transitionProps={{ transition: 'pop-top-right' }}
+                        onClose={() => setUserMenuOpened(false)}
+                        onOpen={() => setUserMenuOpened(true)}
+                        withinPortal
+                    >
+                        {/*<Menu.Target>*/}
+                        {/*    <UnstyledButton*/}
+                        {/*        className={cx(classes.user, { [classes.userActive]: userMenuOpened })}*/}
+                        {/*    >*/}
+                        {/*        <Group gap={7}>*/}
+                        {/*            <Avatar src={user.image} alt={user.name} radius="xl" size={20} />*/}
+                        {/*            <Text fw={500} size="sm" lh={1} mr={3}>*/}
+                        {/*                {user.name}*/}
+                        {/*            </Text>*/}
+                        {/*            <IconChevronDown style={{ width: rem(12), height: rem(12) }} stroke={1.5} />*/}
+                        {/*        </Group>*/}
+                        {/*    </UnstyledButton>*/}
+                        {/*</Menu.Target>*/}
+                        <Button
+                            className={classes.userButton}
+                            onClick={() => signOut()}
+                            variant="link"
+                            color="gray"
+                            size={'sm'}
+                        >
+                            Cerrar sesión
+                        </Button>
+                        {/*<Menu.Dropdown>*/}
+                        {/*    <Menu.Item*/}
+                        {/*        leftSection={*/}
+                        {/*            <IconHeart*/}
+                        {/*                style={{ width: rem(16), height: rem(16) }}*/}
+                        {/*                color={theme.colors.red[6]}*/}
+                        {/*                stroke={1.5}*/}
+                        {/*            />*/}
+                        {/*        }*/}
+                        {/*    >*/}
+                        {/*        Favoritos*/}
+                        {/*    </Menu.Item>*/}
+                        {/*    <Menu.Item*/}
+                        {/*        leftSection={*/}
+                        {/*            <IconStar*/}
+                        {/*                style={{ width: rem(16), height: rem(16) }}*/}
+                        {/*                color={theme.colors.yellow[6]}*/}
+                        {/*                stroke={1.5}*/}
+                        {/*            />*/}
+                        {/*        }*/}
+                        {/*    >*/}
+                        {/*        Guardados*/}
+                        {/*    </Menu.Item>*/}
+                        {/*    <Menu.Item*/}
+                        {/*        leftSection={*/}
+                        {/*            <IconMessage*/}
+                        {/*                style={{ width: rem(16), height: rem(16) }}*/}
+                        {/*                color={theme.colors.blue[6]}*/}
+                        {/*                stroke={1.5}*/}
+                        {/*            />*/}
+                        {/*        }*/}
+                        {/*    >*/}
+                        {/*        Tus comentarios*/}
+                        {/*    </Menu.Item>*/}
+
+                        {/*    <Menu.Label>Settings</Menu.Label>*/}
+                        {/*    <Menu.Item*/}
+                        {/*        leftSection={*/}
+                        {/*            <IconSettings style={{ width: rem(16), height: rem(16) }} stroke={1.5} />*/}
+                        {/*        }*/}
+                        {/*    >*/}
+                        {/*        Configuración de cuenta*/}
+                        {/*    </Menu.Item>*/}
+                        {/*    <Menu.Item*/}
+                        {/*        color="red"*/}
+                        {/*        leftSection={*/}
+                        {/*            <IconLogout style={{ width: rem(16), height: rem(16) }} stroke={1.5} />*/}
+                        {/*        }*/}
+                        {/*        onClick={() => signOut()}*/}
+                        {/*    >*/}
+                        {/*        Cerrar sesión*/}
+                        {/*    </Menu.Item>*/}
+                        {/*</Menu.Dropdown>*/}
+
+                    </Menu>
                 </Group>
-              </UnstyledButton>
-            </Menu.Target>
-            <Menu.Dropdown>
-              <Menu.Item
-                leftSection={
-                  <IconHeart
-                    style={{ width: rem(16), height: rem(16) }}
-                    color={theme.colors.red[6]}
-                    stroke={1.5}
-                  />
-                }
-              >
-                Favoritos
-              </Menu.Item>
-              <Menu.Item
-                leftSection={
-                  <IconStar
-                    style={{ width: rem(16), height: rem(16) }}
-                    color={theme.colors.yellow[6]}
-                    stroke={1.5}
-                  />
-                }
-              >
-                Guardados
-              </Menu.Item>
-              <Menu.Item
-                leftSection={
-                  <IconMessage
-                    style={{ width: rem(16), height: rem(16) }}
-                    color={theme.colors.blue[6]}
-                    stroke={1.5}
-                  />
-                }
-              >
-                Tus comentarios
-              </Menu.Item>
-
-              <Menu.Label>Settings</Menu.Label>
-              <Menu.Item
-                leftSection={
-                  <IconSettings style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-                }
-              >
-                Configuración de cuenta
-              </Menu.Item>
-              <Menu.Item
-                color="red"
-                leftSection={
-                  <IconLogout style={{ width: rem(16), height: rem(16) }} stroke={1.5} />
-                }
-                onClick={() => signOut()}
-              >
-                Cerrar sesión
-              </Menu.Item>
-            </Menu.Dropdown>
-          </Menu>
-        </Group>
-      </Container>
-      <Container size="md">
-        <Tabs
-          defaultValue="Mercado"
-          variant="outline"
-          visibleFrom="sm"
-          classNames={{
-            root: classes.tabs,
-            list: classes.tabsList,
-            tab: classes.tab,
-          }}
-        >
-          <Tabs.List>{items}</Tabs.List>
-        </Tabs>
-      </Container>
-    </div>
-  );
+            </Container>
+            <Container size="md">
+                <Tabs
+                    defaultValue="Mercado"
+                    variant="outline"
+                    visibleFrom="sm"
+                    classNames={{
+                        root: classes.tabs,
+                        list: classes.tabsList,
+                        tab: classes.tab,
+                    }}
+                >
+                    <Tabs.List>{items}</Tabs.List>
+                </Tabs>
+            </Container>
+        </div>
+    );
 };
